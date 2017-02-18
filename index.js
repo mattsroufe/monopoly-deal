d4515f2009548e6b9f8e954327b568fff740376c
Matt Sroufe <mattsroufe@gmail.com>
2017-02-18 19:40:35 +1300

    adds socket.io

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+  console.log(socket.client.conn.id);
+  io.emit('data', 'hello world');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+  console.log('listening on localhost:3000');
 });
-
