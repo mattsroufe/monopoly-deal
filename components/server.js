563dd02cf1f5d3af1494c75201b48a4e2e1c2e8f
Matt Sroufe <mattsroufe@gmail.com>
2017-02-18 22:15:49 +1300

    moves socketio into server.js

--- components/server.js
+++ components/server.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var io = require('socket.io-client');
+
+var socket = io();
+socket.on('data', function (data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <h1>Server App</h1>,
   document.getElementById('app')
 );
